5526845a840efaa467ed80e7c5df3aeefe9856d6
Sapna Blesson <sapnab@cybcube.com>
2020-09-08 12:29:16 -0700

    init

---
+++ script.sh
@@ -0,0 +1,3 @@
+export PATH=/opt/apache-maven-3.5.2/bin:/Library/Java/JavaVirtualMachines/jdk1.8.0_152.jdk/Contents/Home:$PATH
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_152.jdk/Contents/Home
+export MAVEN_HOME=/opt/apache-maven-3.5.2
